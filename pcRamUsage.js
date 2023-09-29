e4d0de23d1f9a595c569aea0d9b2a215902b9b2e
Amanda De Moura <amandstudy@gmail.com>
2023-09-29 19:02:45 -0300

    seeing the RAM usage real-time

--- pcRamUsage.js
+++ pcRamUsage.js
@@ -1,14 +1,27 @@
-const os = require('os');
+const os = require("os");
 
-const {arch, platform, totalmem, freemem} = os;
-const tRam = totalmem()/1024/1024;
-const fRam = freemem()/1024/1024;
+//essa função setInterval recebe dois parametros, uma função,
+//e a quantidade de tempo em que ele vai demorar para executar essa função novamente, em milisegundos
+// colocando 1000 milisegundos, quer dizer que ele vai executar essa função novamente a cada 1s
 
-const stats = {
+//ver o uso da memoria ram em tempo real
+
+setInterval(() => {
+  const { arch, platform, totalmem, freemem } = os;
+  const tRam = totalmem() / 1024 / 1024;
+  const fRam = freemem() / 1024 / 1024;
+  const usage = 100 - ((fRam/tRam) * 100);
+
+  const stats = {
     OS: platform(),
     Arch: arch(),
-    TotalRAM: parseInt(tRam),
-    FreeRAM: parseInt(fRam),
-}
+    TotalRAM: `${parseInt(tRam)}: MB`,
+    FreeRAM: `${parseInt(fRam)}: MB`,
+    UsageRAM: `${usage.toFixed(2)}%`, 
+    //toFixed() é uma função que vai limitar o numero de casas 
+    //deois do ponto, parametro numero que é a quantidade de casas que vc quer ver
+  };
 
-console.log(stats);
+  console.clear();
+  console.table(stats);
+}, 1000);
